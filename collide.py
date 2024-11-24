8854b722021fa69b49fe986778548cbb7f6da9b3
HKledor <114270210+ClayDoors@users.noreply.github.com>
2024-11-24 16:50:36 -0600

    Update collide.py

--- collide.py
+++ collide.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-# Script to craft MD5 collisions of 2 PDFs via mutool and UniColl
+# script to craft MD5 collisions of 2 PDFs via mutool and UniColl
+
 # Ange Albertini 2018-2021
-# Uses mutool from https://mupdf.com/index.html
+
+# uses mutool from https://mupdf.com/index.html
 
 import os
 import sys
@@ -16,28 +18,34 @@ def EnclosedString(d, starts, ends):
     return d[off:d.find(ends, off)]
 
 def getCount(d):
-    s = EnclosedString(d, b"/Count ", b"/")
-    return int(s)
+    try:
+        s = EnclosedString(d, b"/Count ", b"/")
+        return int(s.decode())  # Ensure it is converted from bytes to integer
+    except (ValueError, AttributeError):
+        print("Error extracting /Count value.")
+        return 0
 
 def procreate(l):  # :p
     return b" 0 R ".join(l) + b" 0 R"
 
 def adjustPDF(contents):
-    """Dumb [start]xref fix: fixes old-school xref with no holes, with hardcoded \\n"""
+    """dumb [start]xref fix: fixes old-school xref with no holes, with hardcoded \\n"""
     startXREF = contents.find(b"\nxref\n0 ") + 1
     endXREF = contents.find(b" \n\n", startXREF) + 1
     origXref = contents[startXREF:endXREF]
     objCount = int(origXref.splitlines()[1].split(b" ")[1])
-    print("Object count: %i" % objCount)
+    print("object count: %i" % objCount)
 
     xrefLines = [
         b"xref",
         b"0 %i" % objCount,
-        b"0000000000 00001 f "  # mutool declares its first xref like this
+        # mutool declare its first xref like this
+        b"0000000000 00001 f "
     ]
 
     i = 1
     while i < objCount:
+        # doesn't support comments at the end of object declarations
         off = contents.find(b"\n%i 0 obj\n" % i) + 1
         xrefLines.append(b"%010i 00000 n " % (off))
         i += 1
@@ -45,13 +53,17 @@ def adjustPDF(contents):
     xref = b"\n".join(xrefLines)
 
     # XREF length should be unchanged
-    assert len(xref) == len(origXref), f"XREF mismatch:\nOld: {origXref}\nNew: {xref}"
+    try:
+        assert len(xref) == len(origXref)
+    except AssertionError:
+        print("<:", repr(origXref))
+        print(">:", repr(xref))
 
     contents = contents[:startXREF] + xref + contents[endXREF:]
 
     startStartXref = contents.find(b"\nstartxref\n", endXREF) + len(b"\nstartxref\n")
     endStartXref = contents.find(b"\n%%%%EOF", startStartXref)
-    contents = contents[:startStartXref] + b"%i" % startXREF + contents[endStartXref:]
+    contents = contents[:startStartXref] + ("%i" % startXREF).encode() + contents[endStartXref:]
 
     return contents
 
@@ -61,9 +73,9 @@ if len(sys.argv) == 1:
     print("Usage: pdf.py <file1.pdf> <file2.pdf>")
     sys.exit()
 
-os.system(f"{MUTOOL} merge -o first.pdf {sys.argv[1]}")
-os.system(f"{MUTOOL} merge -o second.pdf {sys.argv[2]}")
-os.system(f"{MUTOOL} merge -o merged.pdf dummy.pdf {sys.argv[1]} {sys.argv[2]}")
+os.system(MUTOOL + ' merge -o first.pdf %s' % sys.argv[1])
+os.system(MUTOOL + ' merge -o second.pdf %s' % sys.argv[2])
+os.system(MUTOOL + ' merge -o merged.pdf dummy.pdf %s %s' % (sys.argv[1], sys.argv[2]))
 
 with open("first.pdf", "rb") as f:
     d1 = f.read()
@@ -78,64 +90,72 @@ COUNT1 = getCount(d1)
 COUNT2 = getCount(d2)
 
 kids = EnclosedString(dm, b"/Kids[", b"]")
+
+# we skip the first dummy, and the last " 0 R" string
 pages = kids[:-4].split(b" 0 R ")[1:]
+
+KIDS1 = procreate(pages[:COUNT1])
+KIDS2 = procreate(pages[COUNT1:])
 
 template = b"""%%PDF-1.4
 
 1 0 obj
 <<
   /Type /Catalog
+
+  %% for alignments (comments will be removed by merging or cleaning)
   /MD5_is__ /REALLY_dead_now__
   /Pages 2 0 R
+  %% to make sure we don't get rid of the other pages when garbage collecting
   /Fakes 3 0 R
+  %% placeholder for UniColl collision blocks
   /0123456789ABCDEF0123456789ABCDEF012
   /0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0
 >>
 endobj
 
 2 0 obj
-<</Type/Pages/Count %(COUNT2)i/Kids[%(KIDS2)s]>>
+<</Type/Pages/Count %(COUNT2)d/Kids[%(KIDS2)s]>>
 endobj 
 
 3 0 obj
-<</Type/Pages/Count %(COUNT1)i/Kids[%(KIDS1)s]>>
+<</Type/Pages/Count %(COUNT1)d/Kids[%(KIDS1)s]>>
 endobj
 
+%% overwritten - was a fake page to fool merging
 4 0 obj
 << >>
 endobj
+
 """
 
-data = {
-    "COUNT1": COUNT1,
-    "COUNT2": COUNT2,
-    "KIDS1": procreate(pages[:COUNT1]),
-    "KIDS2": procreate(pages[COUNT1:])
-}
-
 contents = template % {
-    b"COUNT1": str(data["COUNT1"]).encode(),
-    b"COUNT2": str(data["COUNT2"]).encode(),
-    b"KIDS1": data["KIDS1"],
-    b"KIDS2": data["KIDS2"]
+    'COUNT1': COUNT1,
+    'COUNT2': COUNT2,
+    'KIDS1': KIDS1.decode(),  # Decode bytes to string
+    'KIDS2': KIDS2.decode()   # Decode bytes to string
 }
 
+# adjust parents for the first set of pages
 contents += dm[dm.find(b"5 0 obj"):].replace(b"/Parent 2 0 R", b"/Parent 3 0 R", COUNT1)
+
+# not necessary (will be fixed by mutool anyway) but avoids warnings
 contents = adjustPDF(contents)
 
 with open("hacked.pdf", "wb") as f:
     f.write(contents)
 
-os.system(f"{MUTOOL} clean -gggg hacked.pdf cleaned.pdf")
+# let's adjust offsets - -g to get rid of object 4 by garbage collecting
+os.system(MUTOOL + ' clean -gggg hacked.pdf cleaned.pdf')
 
 with open("cleaned.pdf", "rb") as f:
     cleaned = f.read()
 
+# some mutool versions do different stuff :(
 cleaned = cleaned.replace(
     b" 65536 f \n0000000016 00000 n \n",
     b" 65536 f \n0000000018 00000 n \n",
-    1
-)
+    1)
 
 with open("pdf1.bin", "rb") as f:
     prefix1 = f.read()
@@ -152,20 +172,23 @@ with open("collision1.pdf", "wb") as f:
 with open("collision2.pdf", "wb") as f:
     f.write(file2)
 
-os.remove("first.pdf")
-os.remove("second.pdf")
-os.remove("merged.pdf")
-os.remove("hacked.pdf")
-os.remove("cleaned.pdf")
+os.remove('first.pdf')
+os.remove('second.pdf')
+os.remove('merged.pdf')
+os.remove('hacked.pdf')
+os.remove('cleaned.pdf')
 
 md5 = hashlib.md5(file1).hexdigest()
+
 assert md5 == hashlib.md5(file2).hexdigest()
 
+# to prove the files should be 100% valid
 print()
-os.system(f"{MUTOOL} info -X collision1.pdf")
+os.system(MUTOOL + ' info -X collision1.pdf')
 print()
-os.system(f"{MUTOOL} info -X collision2.pdf")
+print()
+os.system(MUTOOL + ' info -X collision2.pdf')
 
 print()
-print(f"MD5: {md5}")
+print("MD5: %s" % md5)
 print("Success!")
